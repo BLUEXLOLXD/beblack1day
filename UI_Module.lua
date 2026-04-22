32e9bde6aab71c529f2264da9f6257ba17b6719f
BLUEXLOLXD <116792153+BLUEXLOLXD@users.noreply.github.com>
2026-04-22 16:02:21 +0700

    Create UI_Module.lua

---
+++ UI_Module.lua
@@ -0,0 +1,29 @@
+local UI = {}
+local lp = game:GetService("Players").LocalPlayer
+
+function UI.Setup()
+    local playerGui = lp:WaitForChild("PlayerGui")
+    if playerGui:FindFirstChild("AutoSystemGui") then playerGui.AutoSystemGui:Destroy() end
+
+    local ScreenGui = Instance.new("ScreenGui", playerGui)
+    ScreenGui.Name = "AutoSystemGui"
+    local MainFrame = Instance.new("Frame", ScreenGui)
+    MainFrame.BackgroundColor3 = Color3.fromRGB(15, 15, 20)
+    MainFrame.Position = UDim2.new(0.5, -110, 0.02, 0) 
+    MainFrame.Size = UDim2.new(0, 220, 0, 100)
+    Instance.new("UICorner", MainFrame)
+
+    UI.StatusLabel = Instance.new("TextLabel", MainFrame)
+    UI.StatusLabel.Position = UDim2.new(0, 10, 0, 10)
+    UI.StatusLabel.Size = UDim2.new(1, -20, 1, -20)
+    UI.StatusLabel.BackgroundTransparency = 1
+    UI.StatusLabel.Font = Enum.Font.Gotham
+    UI.StatusLabel.TextColor3 = Color3.fromRGB(0, 255, 180)
+    UI.StatusLabel.TextSize = 11
+    UI.StatusLabel.TextWrapped = true
+    UI.StatusLabel.Text = "Loading UI Module..."
+    
+    return UI
+end
+
+return UI
